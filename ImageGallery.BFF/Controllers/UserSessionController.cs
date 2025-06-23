b39ebacce4622636ccf3a12458f9c4744f8a013a
debkantibarua1979 <129514042+debkantibarua1979@users.noreply.github.com>
2025-06-23 11:52:04 +0600

    Enhance UserSessionController to support token revocation and add IHttpClientFactory dependency

--- ImageGallery.BFF/Controllers/UserSessionController.cs
+++ ImageGallery.BFF/Controllers/UserSessionController.cs
@@ -1,11 +1,25 @@
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 
 namespace ImageGallery.IDP.BFF.Controllers;
 
 public class UserSessionController: Controller
 {
+    private readonly IHttpClientFactory _httpClientFactory;
+    
+    public UserSessionController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory ??
+                             throw new ArgumentNullException(nameof(httpClientFactory));
+    }
+    
     [Authorize]
     public IActionResult GivenName()
     {
@@ -19,6 +33,45 @@ public class UserSessionController: Controller
     
     public async Task Logout()
     {
+        var client = _httpClientFactory.CreateClient("IDPClient");
+        
+        var discoveryDocumentResponse = await client
+            .GetDiscoveryDocumentAsync();
+
+        if (discoveryDocumentResponse.IsError)
+        {
+            throw new Exception(discoveryDocumentResponse.Error);
+        }
+        
+        var accessTokenRevocationResponse = await client
+            .RevokeTokenAsync(new ()
+            {
+                Address = discoveryDocumentResponse.RevocationEndpoint,
+                ClientId = "imagegallerybff",
+                ClientSecret = "anothersecret",
+                Token = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.AccessToken)
+            });
+        
+        if (accessTokenRevocationResponse.IsError)
+        {
+            throw new Exception(accessTokenRevocationResponse.Error);
+        }
+        
+        var refreshTokenRevocationResponse = await client
+            .RevokeTokenAsync(new ()
+            {
+                Address = discoveryDocumentResponse.RevocationEndpoint,
+                ClientId = "imagegallerybff",
+                ClientSecret = "anothersecret",
+                Token = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.RefreshToken)
+            });
+        
+        if (refreshTokenRevocationResponse.IsError)
+        {
+            throw new Exception(refreshTokenRevocationResponse.Error);
+        }
+        
+        
         await HttpContext.SignOutAsync("BFFCookieScheme");
         await HttpContext.SignOutAsync("BFFChallengeScheme");
     }
